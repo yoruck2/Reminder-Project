ddd3e0973625fbfe9842d32a445ca3ad1d3f4036
yoruck2 <yoruck2@gmail.com>
2024-07-06 16:53:19 +0900

    💄priorityLabel,  tagLabel 추가
    및 todoCheckButtonTapped 로직추가

--- Reminder Project/TodoList/TodoListTableViewCell.swift
+++ Reminder Project/TodoList/TodoListTableViewCell.swift
@@ -6,19 +6,41 @@
 //
 
 import UIKit
+import RealmSwift
 
 final class TodoListTableViewCell: BaseTableViewCell {
-    
+    let realm = try! Realm()
     var todoData: TodoListTable? = nil {
         didSet {
             configureView()
         }
     }
     
-    private var todoCheckButton = UIButton().then {
+    private lazy var todoCheckButton = UIButton().then {
         $0.setImage(UIImage(systemName: "circle"), for: .normal)
         $0.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .selected)
         $0.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .highlighted)
+        $0.addTarget(self, action: #selector(todoCheckButtonTapped(_:)), for: .touchUpInside)
+    } {
+        didSet {
+            print("asf")
+        }
+    }
+    @objc
+    func todoCheckButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        try? todoData?.realm?.write {
+            if sender.isSelected {
+                todoData?.isDone = true
+            } else {
+                todoData?.isDone = false
+            }
+            
+        }
+    }
+    
+    private var priorityLabel = UILabel().then {
+        $0.textColor = .systemBlue
     }
     private var todoNameLabel = UILabel().then {
         $0.textColor = .white
@@ -29,21 +51,31 @@ final class TodoListTableViewCell: BaseTableViewCell {
     private var deadlineLabel = UILabel().then {
         $0.textColor = .lightGray
     }
+    private var tagLabel = UILabel().then {
+        $0.textColor = .link
+    }
     
     override func configureHierarchy() {
+        contentView.addSubview(priorityLabel)
         contentView.addSubview(todoCheckButton)
         contentView.addSubview(todoNameLabel)
         contentView.addSubview(todoMemoLabel)
         contentView.addSubview(deadlineLabel)
+        contentView.addSubview(tagLabel)
     }
     override func configureLayout() {
-        backgroundColor = .black
+        
+        
         todoCheckButton.snp.makeConstraints { make in
             make.leading.top.equalTo(contentView.safeAreaLayoutGuide).inset(10)
         }
-        todoNameLabel.snp.makeConstraints { make in
+        priorityLabel.snp.makeConstraints { make in
             make.leading.equalTo(todoCheckButton.snp.trailing).offset(10)
             make.top.equalTo(todoCheckButton)
+        }
+        todoNameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(priorityLabel.snp.trailing)
+            make.top.equalTo(priorityLabel)
         }
         todoMemoLabel.snp.makeConstraints { make in
             make.leading.equalTo(todoNameLabel)
@@ -53,14 +85,24 @@ final class TodoListTableViewCell: BaseTableViewCell {
             make.leading.equalTo(todoMemoLabel)
             make.top.equalTo(todoMemoLabel.snp.bottom).offset(5)
         }
+        tagLabel.snp.makeConstraints { make in
+            make.top.equalTo(deadlineLabel)
+            make.leading.equalTo(deadlineLabel.snp.trailing)
+        }
     }
     override func configureView() {
+        backgroundColor = .black
+        
         guard let todoData else {
             return
         }
         todoCheckButton.isSelected = todoData.isDone
         todoNameLabel.text = todoData.name
         todoMemoLabel.text = todoData.memo
-        deadlineLabel.text = todoData.deadline
+        deadlineLabel.text = "\(todoData.deadline?.toString ?? "") "
+        priorityLabel.text = "\(String(repeating: "!", count: todoData.priority ?? 0)) "
+        if todoData.tag != "" {
+            tagLabel.text = " \(todoData.tag ?? "")"
+        }
     }
 }
