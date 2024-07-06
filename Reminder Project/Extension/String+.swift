c1526b1221a94d2897446b87513757097b293db8
yoruck2 <yoruck2@gmail.com>
2024-07-06 16:48:49 +0900

    🩹 toPriority 수정
    우선순위에 따른 느낌표 갯수 리턴

--- Reminder Project/Extension/String+.swift
+++ Reminder Project/Extension/String+.swift
@@ -8,17 +8,26 @@
 import Foundation
 
 extension String {
-    
+    var toDate: Date? {
+        let birthdayFormatStyle = Date.FormatStyle()
+            .year(.defaultDigits)
+            .month(.twoDigits)
+            .day(.twoDigits)
+            .weekday(.abbreviated)
+            .locale(Locale(identifier: "ko_KR"))
+        return try? birthdayFormatStyle.parse(self)
+    }
+
     var toPriority: Int {
         switch self{
         case "상":
-            return 0
-        case "중":
             return 1
-        case "하":
+        case "중":
             return 2
+        case "하":
+            return 3
         default:
-            return 0
+            return 4
         }
     }
 }
