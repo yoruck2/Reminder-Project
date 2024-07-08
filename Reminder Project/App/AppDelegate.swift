25e4ad81dc85534ee18906f9d79ed1d27b92756b
yoruck2 <yoruck2@gmail.com>
2024-07-09 04:49:14 +0900

    ✨ 스키마 버전 관리 추가

--- Reminder Project/App/AppDelegate.swift
+++ Reminder Project/App/AppDelegate.swift
@@ -6,10 +6,24 @@
 //
 
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let config = Realm.Configuration(schemaVersion: 2) { migration, oldSchemaVersion in
+            
+            if oldSchemaVersion < 1 {
+
+            }
+            
+            if oldSchemaVersion < 2 {
+                // 
+            }
+            
+        }
+        
+        Realm.Configuration.defaultConfiguration = config
 
         return true
     }
